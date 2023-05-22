9ce09a77cd3807df5acf69b92fd3e7952a5fbbdd
TisenkoVladislavAI216 <113099644+TisenkoVladislavAI216@users.noreply.github.com>
2023-05-23 01:54:29 +0300

    Add files via upload

---
+++ main/authorization.php
@@ -0,0 +1,26 @@
+<?php
+  $login = filter_var(trim($_POST['login']),
+  FILTER_SANITIZE_STRING);
+
+  $pass = filter_var(trim($_POST['pass']),
+  FILTER_SANITIZE_STRING);
+
+  $pass = md5($pass);
+
+  $mysql = new mysqli('localhost', 'ADMIN', 'ADMIN', 'ADMIN');
+
+  $result = $mysql->query("SELECT * FROM `game_haven_users`
+  WHERE `login` = '$login' AND `pass` = '$pass'");
+
+  $user = $result->fetch_assoc();
+
+  if(count($user) == 0) {
+    echo "Такого користувача не знайдено!";
+    exit;
+  }
+
+  setcookie('user', $user['name'], time() + 60*60, "/");
+
+  $mysql->close();
+  header('Location: /main.php');
+?>
