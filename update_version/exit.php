0227bfd7bca6f7ffa085dad2ec3bcb87e7eca0be
Protaga <101981115+Protaga@users.noreply.github.com>
2023-06-09 20:59:50 +0300

    Delete update_version directory

--- update_version/exit.php
+++
@@ -1,7 +0,0 @@
-<?php
-setcookie('user', $user['name'], time() - 60*60*12, "/");
-setcookie('cookieLogin', $user['login'], time() - 60*60*12, "/");
-setcookie('witcher_key', $user['witcher_key'], time() - 60*60*12, "/");
-
-header('Location: ./main.php')
-  ?>
--- update_version/js/close_pay_content.js
+++
@@ -1,7 +0,0 @@
-const close_cross_container = document.getElementById('close_cross_container');
-const close_pay_content = document.getElementById('pay_content');
-
-close_cross_container.addEventListener('click', function(event) {
-    event.preventDefault();
-    close_pay_content.style.display = 'none';
-});
